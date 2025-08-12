714686ac369929c070edfbf13c6302af12fa72f7
SmallMain <smallmain@outlook.com>
2025-08-12 23:34:16 +0800

    feat: add array exports for script, javascript, typescript, css, html, vue, and test suffixes

--- glob.js
+++ glob.js
@@ -6,11 +6,38 @@
 export const scriptExt = `{js?(x),ts?(x),mts?(x),mjs?(x),cts?(x),cjs?(x)}`;
 
 /**
+ * 脚本文件扩展名数组
+ *
+ * @type {string[]}
+ */
+export const scriptExts = [
+  'js',
+  'jsx',
+  'ts',
+  'tsx',
+  'mts',
+  'mtsx',
+  'mjs',
+  'mjsx',
+  'cts',
+  'ctsx',
+  'cjs',
+  'cjsx',
+];
+
+/**
  * JS 脚本文件扩展名
  *
  * @type {string}
  */
 export const javascriptExt = `{js?(x),mjs?(x),cjs?(x)}`;
+
+/**
+ * JS 脚本文件扩展名数组
+ *
+ * @type {string[]}
+ */
+export const javascriptExts = ['js', 'jsx', 'mjs', 'mjsx', 'cjs', 'cjsx'];
 
 /**
  * TS 脚本文件扩展名
@@ -20,11 +47,25 @@ export const javascriptExt = `{js?(x),mjs?(x),cjs?(x)}`;
 export const typescriptExt = `{ts?(x),mts?(x),cts?(x)}`;
 
 /**
+ * TS 脚本文件扩展名数组
+ *
+ * @type {string[]}
+ */
+export const typescriptExts = ['ts', 'tsx', 'mts', 'mtsx', 'cts', 'ctsx'];
+
+/**
  * CSS 文件扩展名
  *
  * @type {string}
  */
 export const cssExt = `{css,scss,sass,less,pcss}`;
+
+/**
+ * CSS 文件扩展名数组
+ *
+ * @type {string[]}
+ */
+export const cssExts = ['css', 'scss', 'sass', 'less', 'pcss'];
 
 /**
  * HTML 文件扩展名
@@ -34,6 +75,13 @@ export const cssExt = `{css,scss,sass,less,pcss}`;
 export const htmlExt = `{htm,html}`;
 
 /**
+ * HTML 文件扩展名数组
+ *
+ * @type {string[]}
+ */
+export const htmlExts = ['htm', 'html'];
+
+/**
  * Vue 文件扩展名
  *
  * @type {string}
@@ -41,8 +89,22 @@ export const htmlExt = `{htm,html}`;
 export const vueExt = `{vue}`;
 
 /**
+ * Vue 文件扩展名数组
+ *
+ * @type {string[]}
+ */
+export const vueExts = ['vue'];
+
+/**
  * 测试脚本文件名后缀
  *
  * @type {string}
  */
 export const testSuffix = `{test,spec}`;
+
+/**
+ * 测试脚本文件名后缀数组
+ *
+ * @type {string[]}
+ */
+export const testSuffixs = ['test', 'spec'];
--- types/glob.d.ts
+++ types/glob.d.ts
@@ -5,11 +5,23 @@
  */
 export const scriptExt: string;
 /**
+ * 脚本文件扩展名数组
+ *
+ * @type {string[]}
+ */
+export const scriptExts: string[];
+/**
  * JS 脚本文件扩展名
  *
  * @type {string}
  */
 export const javascriptExt: string;
+/**
+ * JS 脚本文件扩展名数组
+ *
+ * @type {string[]}
+ */
+export const javascriptExts: string[];
 /**
  * TS 脚本文件扩展名
  *
@@ -17,11 +29,23 @@ export const javascriptExt: string;
  */
 export const typescriptExt: string;
 /**
+ * TS 脚本文件扩展名数组
+ *
+ * @type {string[]}
+ */
+export const typescriptExts: string[];
+/**
  * CSS 文件扩展名
  *
  * @type {string}
  */
 export const cssExt: string;
+/**
+ * CSS 文件扩展名数组
+ *
+ * @type {string[]}
+ */
+export const cssExts: string[];
 /**
  * HTML 文件扩展名
  *
@@ -29,14 +53,32 @@ export const cssExt: string;
  */
 export const htmlExt: string;
 /**
+ * HTML 文件扩展名数组
+ *
+ * @type {string[]}
+ */
+export const htmlExts: string[];
+/**
  * Vue 文件扩展名
  *
  * @type {string}
  */
 export const vueExt: string;
 /**
+ * Vue 文件扩展名数组
+ *
+ * @type {string[]}
+ */
+export const vueExts: string[];
+/**
  * 测试脚本文件名后缀
  *
  * @type {string}
  */
 export const testSuffix: string;
+/**
+ * 测试脚本文件名后缀数组
+ *
+ * @type {string[]}
+ */
+export const testSuffixs: string[];
